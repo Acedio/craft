ce55160cd5796135b04a9b2cd8094401b69a81f5
Josh Simmons <josh.simmons@gmail.com>
2009-05-01 13:47:09 -0700

    Lalalala jason is weird.

--- gridmap.cpp
+++ gridmap.cpp
@@ -1,7 +1,16 @@
+#include <queue>
+#include <stack>
+#include <vector>
+#include <set>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
 #include "gridmap.h"
 #include "objectmanager.h"
+#include "globals.h"
 
-GridMap::GridMap(int w, int h, int def){
+GridMap::GridMap(int w, int h, ObjectRef def){
 	object_map = new ObjectRef[w*h];
 	for(int i = 0; i < w*h; i++){
 		object_map[i] = def;
@@ -12,4 +21,37 @@ GridMap::~GridMap(){
 	if(object_map != NULL){
 		delete[] object_map;
 	}
+}
+
+stack<PointI> GridMap::AStar(PointI a, PointI b){
+	priority_queue<AStarPoint*,vector<AStarPoint*>,ASPComp> open;
+	set<AStarPoint*> closed;
+	AStarPoint* start = new AStarPoint;
+	start->point = a;
+	start->parent = NULL;
+	start->d = 0;
+	start->rank = 0;
+	open.push(start);
+	AStarPoint* cur;
+	while(!open.empty() && (cur = open.top()->point) != b){
+		for(int d = 0; d < 4; ++d){
+			AStarPoint* n = new AStarPoint;
+			PointI temp;
+			temp.x = cur->point.x + 2*(d&1) - 1;
+			temp.y = cur->point.y + (d&2) - 1;
+			n->point = temp;
+			n->d = cur->d+1;
+			n->rank = d + abs(b.x-n->point.x) + abs(b.y-n->point.y);
+		}
+	}
+	return stack<PointI>();
+}
+
+int main(){
+	GridMap g(10,10,0);
+	PointI a, b;
+	a.x = a.y = 1;
+	b.x = b.y = 2;
+	g.AStar(a,b);
+	return 0;
 }
--- gridmap.h
+++ gridmap.h
@@ -1,14 +1,28 @@
 #ifndef _GRIDMAP_H_
 #define _GRIDMAP_H_
 
+#include <list>
+
 #include "objectmanager.h"
+#include "globals.h"
+
+struct AStarPoint{
+	PointI point;
+	PointI* parent;
+	int d,rank;
+};
+
+class ASPComp{
+public:
+	bool operator() (AStarPoint* a, AStarPoint* b){return a->rank > b->rank;}
+};
 
 class GridMap{
 public:
-	GridMap(int w, int h, int def = 0);
+	GridMap(int w, int h, ObjectRef def = 0);
 	~GridMap();
 
-	void Draw();
+	stack<PointI> AStar(PointI start, PointI end);
 private:
 	ObjectRef* object_map;
 };
