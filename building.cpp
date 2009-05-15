3e4923259fb169470c39f32dd37042d195cfc12e
dajaseon <jsimmons11@whitworth.edu>
2009-05-15 09:22:48 -0700

    COOL

--- building.cpp
+++ building.cpp
@@ -2,11 +2,11 @@
 #include "object.h"
 #include "building.h"
 
-Building::Building(){
+Building::Building(ModelManager* modelManager,TextureManager* textureManager,int x,int y){
 	pos.x = 0;
 	pos.y = 0;
 	model = 0;
-	buildtime = 0;
+	build_time = 0;
 	max_hp;
 	cur_hp = 1;
 	defense = 20;
@@ -15,7 +15,7 @@ Building::Building(){
 Building::Building(PointI p){
 	pos = p;
 	model = 0;
-	buildtime = 0;
+	build_time = 0;
 	max_hp;
 	cur_hp = 1;
 	defense = 20;
